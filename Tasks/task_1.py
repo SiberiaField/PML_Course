e1b7a73ae5456f2bb86131e2d8f89b5cc7e09d25
Vlad Polyanskii <polyanskij.06@list.ru>
2025-09-25 20:40:43 +0700

    Made more common solution for Task 1

--- Tasks/task_1.py
+++ Tasks/task_1.py
@@ -1,16 +1,11 @@
-import os
+from json import loads
+from argparse import ArgumentParser
 
-from dotenv import load_dotenv
 import pandas as pd
 
 
-# Читаем DATA_URL из .env файла
-load_dotenv()
-
-
-# Так как Boston датасет не содрежит имён колонок,
-# вот один из варинатов наименования.
-COL_NAMES = [
+# Default col names for Boston dataset
+BOSTON_COL_NAMES = [
     "Potato",
     "Tomato",
     "Cabbage",
@@ -25,21 +20,13 @@ COL_NAMES = [
 ]
 
 
-def safe_read_csv() -> pd.DataFrame | None:
+def safe_read_csv(read_kwargs: dict) -> pd.DataFrame | None:
     """
-    This function reads dataframe from DATA_URL environment variable.\n
+    This function reads dataframe using given kwargs which are passed directly into pandas.read_csv function.\n
     It returns pd.DataFrame if there are no troubles and it returns None if there are.
     """
-    url = os.getenv("DATA_URL")
-    if url is None or url == "":
-        print("Переменная окружения DATA_URL не определена или содержит пустую строку")
-        return None
-
     try:
-        # Параметры этого метода, в частности, skiprows=22,
-        # были взяты из рекомендации от sklearn по установке датасета Boston.
-        # То есть, эти параметры подобраны исключительно под Boston датасет.
-        df = pd.read_csv(url, sep="\s+", skiprows=22, header=None, names=COL_NAMES)
+        df = pd.read_csv(**read_kwargs)
         if df.empty:
             print("Файл пустой или не содержит данных")
         return df
@@ -54,8 +41,22 @@ def safe_read_csv() -> pd.DataFrame | None:
         return None
 
 
+def parse_args():
+    parser = ArgumentParser(
+        description="This program reads dataset via pandas.read_csv function, computes variances over each column and prints the three highest results")
+    parser.add_argument("--read_csv_kwargs", type=loads,
+                        help="Parameters for read_csv function. Refer to pandas.read_csv documentation for more information. Default is parameters for Boston dataset",
+                        default={"filepath_or_buffer": "http://lib.stat.cmu.edu/datasets/boston",
+                                 "sep": "\\s+",
+                                 "skiprows": 22,
+                                 "header": None,
+                                 "names": BOSTON_COL_NAMES})
+    return parser.parse_args()
+
+
 def main():
-    raw_df = safe_read_csv()
+    read_csv_kwargs = parse_args().read_csv_kwargs
+    raw_df = safe_read_csv(read_csv_kwargs)
     if raw_df is not None:
         df = raw_df.dropna()
         df_vars = df.var(numeric_only=True)
